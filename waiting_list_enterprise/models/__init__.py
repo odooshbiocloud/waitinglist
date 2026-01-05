e54f37fea1e9ef21ae9bc20e7c65f4f59f9ae25a
odooshbiocloud <odooshbiocloud@gmail.com>
2026-01-05 10:44:50 +0400

    Init

---
+++ waiting_list_enterprise/models/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+from . import waiting_list
+from . import waiting_list_notification
+from . import restaurant_table
+from . import res_config_settings
+from . import res_users
