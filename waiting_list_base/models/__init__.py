99c33a14f99911e0669364c2a319f7ef50c9d866
odooshbiocloud <odooshbiocloud@gmail.com>
2026-01-07 03:57:26 +0400

    Delete waiting_list_base directory

--- waiting_list_base/models/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import waiting_list_allergen
-from . import waiting_list
-from . import res_partner
-from . import res_config_settings
-from . import waiting_list_customer_wizard
