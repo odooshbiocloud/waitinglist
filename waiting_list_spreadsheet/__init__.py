42e985f6aa91c60e62a59e5cf31ff995bd3277c9
odooshbiocloud <odooshbiocloud@gmail.com>
2026-01-07 03:57:54 +0400

    Delete waiting_list_spreadsheet directory

--- waiting_list_spreadsheet/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import models
-
-def post_init_hook(env):
-    """Create default spreadsheet template after module installation"""
-    env['waiting.list.spreadsheet']._create_default_template()
